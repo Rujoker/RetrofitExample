a94191c0c634b3bff664177d9f06e63f93906b45
sergeypchelintsev <veselyrojer@mail.ru>
2018-03-07 13:45:39 +0300

    1

--- app/src/main/java/com/example/sergeypchelintsev/retrofitexample/MainActivity.java
+++ app/src/main/java/com/example/sergeypchelintsev/retrofitexample/MainActivity.java
@@ -11,22 +11,28 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.button)
     Button button;
-    @BindView(R.id.query)
-    EditText query;
-    @BindView(R.id.result)
-    TextView result;
+    @BindView(R.id.city)
+    EditText city;
+    @BindView(R.id.summary)
+    TextView summary;
+    @BindView(R.id.today)
+    TextView today;
 
-    private String BASE_URL = "http://samples.openweathermap.org" ;
+    private String BASE_URL = "http://samples.openweathermap.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +41,48 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+
+    }
+
+    @OnClick (R.id.button)
+    public void onClick(View view) {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build();
+
         Retrofit client = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         OpenWeatherAPI service = client.create(OpenWeatherAPI.class);
-        Call<GithubUser> call = service.getCity("524901");
+        Call<WeatherResponse> call = service.getCity(city.getText().toString());
 
-        call.enqueue(new Callback<GithubUser>() {
+        call.enqueue(new Callback<WeatherResponse>() {
             @Override
-            public void onResponse(Call<GithubUser> call, Response<GithubUser> response) {
+            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
                 Toast.makeText(MainActivity.this, String.valueOf(response.code()), Toast.LENGTH_SHORT).show();
                 if (response.isSuccessful()) {
                     // request successful (status code 200, 201)
-                    GithubUser result1 = response.body();
-                    result.setText(result1.toString());
+                    WeatherResponse result = response.body();
+
+                    String sum;
+                    String str;
+                    str = "";
+                    sum = "City: " + result.getCity().getName() + ", Country: " + result.getCity().getCountry();
+                    for (int i = 0; i < 1; i++) {
+                        str = "Time: " + result.getList().get(i).getDtTxt() +
+                                ", Description: " + result.getList().get(i).getWeather().get(0).getDescription() +
+                                ", Temp: " + result.getList().get(i).getMain().getTemp() +
+                                ", Maximal: " + result.getList().get(i).getMain().getTempMax() +
+                                ", Minimal: " + result.getList().get(i).getMain().getTempMin() +
+                                ", Pressure: " + result.getList().get(i).getMain().getPressure();
+                    }
+
+                    summary.setText(sum);
+                    today.setText(str);
 
                 } else {
                     //request not successful (like 400,401,403 etc)
@@ -58,72 +90,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+
             @Override
-            public void onFailure(Call<GithubUser> call, Throwable t) {
+            public void onFailure(Call<WeatherResponse> call, Throwable t) {
 
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
     }
-
-
-
-
-    @OnClick (R.id.button)
-    public void onClick(View view) {
-    }
-
-
-
-
-
-//    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-//
-//        private Exception exception;
-//
-//        protected void onPreExecute() {
-//            result.setText("");
-//        }
-//
-//        protected String doInBackground(Void... urls) {
-//            String login = query.getText().toString();
-//            // Do some validation here
-//
-//            try {
-//                URL url = new URL(API_URL + login);
-//                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-//                try {
-//                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-//                    StringBuilder stringBuilder = new StringBuilder();
-//                    String line;
-//                    while ((line = bufferedReader.readLine()) != null) {
-//                        stringBuilder.append(line).append("\n");
-//                    }
-//                    bufferedReader.close();
-//                    return stringBuilder.toString();
-//                }
-//                finally{
-//                    urlConnection.disconnect();
-//                }
-//            }
-//            catch(Exception e) {
-//                Log.e("ERROR", e.getMessage(), e);
-//                return null;
-//            }
-//        }
-//
-//        protected void onPostExecute(String response) {
-//            if(response == null) {
-//                response = "THERE WAS AN ERROR";
-//            }
-//            Log.i("INFO", response);
-//            result.setText(response);
-//        }
-//    }
-
-
-
 }
+
--- app/src/main/java/com/example/sergeypchelintsev/retrofitexample/OpenWeatherAPI.java
+++ app/src/main/java/com/example/sergeypchelintsev/retrofitexample/OpenWeatherAPI.java
@@ -3,6 +3,7 @@ package com.example.sergeypchelintsev.retrofitexample;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by sergeypchelintsev on 06.03.2018.
@@ -10,6 +11,6 @@ import retrofit2.http.Path;
 
 public interface OpenWeatherAPI {
 
-    @GET("/data/2.5/forecast?id={city_ID}")
-    Call<GithubUser> getCity(@Path("city_ID") String city_ID);
+    @GET("/data/2.5/forecast?appid=f4540f77f79170327fc25b3039261c45")
+    Call<WeatherResponse> getCity(@Query("id") String city_ID);
 }
